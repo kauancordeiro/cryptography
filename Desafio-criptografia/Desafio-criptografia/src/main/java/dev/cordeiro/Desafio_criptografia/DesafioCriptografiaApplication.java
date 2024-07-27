e67cf20bc25bc8580d30841077923e8f23c64f81
Kauan <cvitorcordeiro@gmail.com>
2024-07-26 22:59:04 -0300

    first commit

---
+++ Desafio-criptografia/Desafio-criptografia/src/main/java/dev/cordeiro/Desafio_criptografia/DesafioCriptografiaApplication.java
@@ -0,0 +1,13 @@
+package dev.cordeiro.Desafio_criptografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCriptografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCriptografiaApplication.class, args);
+	}
+
+}
---
+++ Desafio-criptografia/Desafio-criptografia/src/test/java/dev/cordeiro/Desafio_criptografia/DesafioCriptografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cordeiro.Desafio_criptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
