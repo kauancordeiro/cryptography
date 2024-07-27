4827727f12332a41ebbf95c68b612cb728c4a32a
Kauan <cvitorcordeiro@gmail.com>
2024-07-27 15:30:21 -0300

    prePersist and postLoad

--- Desafio-criptografia/Desafio-criptografia/src/main/java/dev/cordeiro/Desafio_criptografia/entities/TransactionalEntity.java
+++ Desafio-criptografia/Desafio-criptografia/src/main/java/dev/cordeiro/Desafio_criptografia/entities/TransactionalEntity.java
@@ -1,9 +1,18 @@
 package dev.cordeiro.Desafio_criptografia.entities;
 
+import dev.cordeiro.Desafio_criptografia.service.CyptoService;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_transactional")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionalEntity {
 
 
@@ -13,12 +22,29 @@ public class TransactionalEntity {
     private long transactionalId;
 
     @Column(name = "user_document")
-    private String userDocument;
+    private String encryptedUserDocument;
 
     @Column(name = "credit_card_token")
-    private String creditCardToken;
+    private String encryptedCreditCardToken;
 
     @Column(name = "transactional_value")
     private long transactionalValue;
+
+    @Transient
+    private String rawUserDocument;
+
+    @Transient
+    private String rawCreditCardToken;
+
+    @PrePersist
+    public void prePersist(){
+        this.encryptedUserDocument = CyptoService.encrypt(rawUserDocument);
+        this.encryptedCreditCardToken = CyptoService.encrypt(rawCreditCardToken);
+    }
+    @PostLoad
+    public void postLoad(){
+        this.rawUserDocument = CyptoService.decrypt(encryptedUserDocument);
+        this.rawCreditCardToken = CyptoService.decrypt(encryptedCreditCardToken);
+    }
 
 }
